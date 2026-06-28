13964b4535e6b078c3e00b4e0de35abefb8633f6
John Manuel Carado <carado.johnp11q@gmail.com>
2026-06-28 20:10:39 +0800

    Add bud init click func

--- main.py
+++ main.py
@@ -1,3 +1,18 @@
+import os
+
+import click
+
+
+@click.command()
+def init():
+    if os.path.exists("bud.json"):
+        click.echo("Error: bud.json already exists in the current directory.")
+        return
+
+    with open("bud.json", "w") as f:
+        f.write("{}")
+
+
 def main():
     print("Hello from bud!")
 
